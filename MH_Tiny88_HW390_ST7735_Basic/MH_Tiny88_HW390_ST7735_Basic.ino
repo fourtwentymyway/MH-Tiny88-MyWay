4f0628d451a10210408213ba933804bab7cac27a
FourTwenty MyWay <145503735+fourtwentymyway@users.noreply.github.com>
2026-01-10 15:59:10 -0800

    Update MH_Tiny88_HW390_ST7735_Basic.ino

--- MH_Tiny88_HW390_ST7735_Basic/MH_Tiny88_HW390_ST7735_Basic.ino
+++ MH_Tiny88_HW390_ST7735_Basic/MH_Tiny88_HW390_ST7735_Basic.ino
@@ -7,17 +7,17 @@ PDQ_ST7735 tft;			// PQ: create LCD object (using pins in "PDQ_ST7735_config.h")
 void setup()
 {
 
-  tft.begin();
+ tft.begin();
 	tft.setRotation(3);
-  tft.invertDisplay(1);
+ tft.invertDisplay(1);
 	tft.fillScreen(ST7735_BLUE);
-  tft.fillRect(6, 40, 150, 70, ST7735_BLACK); // X sTART,Y CENTER,  Xpx LONG, Y HEIGHT
+ tft.fillRect(6, 40, 150, 70, ST7735_BLACK); // X sTART,Y CENTER,  Xpx LONG, Y HEIGHT
 	//digitalWrite(ST7735TFT_RST, LOW); delay(100);
-  tft.setCursor(125, 20);
+ tft.setCursor(125, 20);
 	tft.setTextSize(3);
 	tft.setTextColor(ST7735_WHITE);
 	tft.print( "    420 " );
-  tft.setCursor(125, 43);
+ tft.setCursor(125, 43);
 	tft.setTextSize(3);
 	tft.setTextColor(ST7735_WHITE);
 	tft.print( "   MyWAY " );
@@ -25,7 +25,7 @@ void setup()
 	tft.fillScreen(ST7735_BLUE); //Needed To Clear Screen Stop Flickering
 	tft.fillRect(6, 30, 150, 70, ST7735_BLACK);
 	pinMode(LCD_BACKLIGHT_PIN, OUTPUT); // Set the pin as an output
-  digitalWrite(LCD_BACKLIGHT_PIN, HIGH); // Turn the backlight ON (HIGH is typically ON)
+ digitalWrite(LCD_BACKLIGHT_PIN, HIGH); // Turn the backlight ON (HIGH is typically ON)
   // For brightness control, you would use analogWrite(LCD_BACKLIGHT_PIN, value);
   
 };
@@ -44,6 +44,7 @@ void loop() {
 	tft.setTextSize(3);
 	tft.setTextColor(ST7735_YELLOW);
 	tft.print(sensorValue);      // Print the value to the Serial Monitor
-  delay(500);                       // Wait for half a second
+ 
+ delay(500);                       // Wait for half a second
 }
 
