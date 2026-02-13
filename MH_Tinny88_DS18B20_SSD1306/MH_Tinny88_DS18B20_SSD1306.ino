41f48cfb4822d0db039ba7778c90adf1e99a1fd7
FourTwenty MyWay <145503735+fourtwentymyway@users.noreply.github.com>
2026-02-12 18:27:13 -0800

    Update MH_Tinny88_DS18B20_SSD1306.ino

--- MH_Tinny88_DS18B20_SSD1306/MH_Tinny88_DS18B20_SSD1306.ino
+++ MH_Tinny88_DS18B20_SSD1306/MH_Tinny88_DS18B20_SSD1306.ino
@@ -2,46 +2,48 @@
 #include <DallasTemperature.h>
 #include <TinyI2CMaster.h>
 #include <Tiny4kOLED.h>
-#include "5x5_font.h"
-const DCfont *currentFont = FONT5X5;
+//#include "5x5_font.h"
+#include "pzim3x5_font.h"
 
-#define ONE_WIRE_BUS_1 3
+//const DCfont *currentFont = FONT5X5;
+const DCfont *currentFont = FONTPZIM3X5;
 
+#define ONE_WIRE_BUS 3
 
-OneWire oneWire_in(ONE_WIRE_BUS_1);
+OneWire oneWire(ONE_WIRE_BUS);
+ 
+DallasTemperature sensors(&oneWire);
 
-
-DallasTemperature sensor_inhouse(&oneWire_in);
-
-
-void setup(void)
-{
+void setup(){
   oled.begin(72, 40, sizeof(tiny4koled_init_72x40br), tiny4koled_init_72x40br);
   oled.setFont(currentFont);
+  oled.setSpacing(currentFont->spacing + 1); //3x5Font Uncomment For 5x5FONT
   oled.clear();
   oled.on(); 
-  oled.setCursor(17, 2);
-  oled.println("DS18B20");
-  sensor_inhouse.begin();
-  delay(800);
-  oled.clear();
+  sensors.begin();
 }
 
-void loop(void)
-{
-  oled.setFont(currentFont);
-  sensor_inhouse.requestTemperatures();
-  oled.setCursor(15, 0);
-  oled.print("degrees");
-  oled.setCursor(4, 1);
-  oled.print("fahrenheit:");
-  oled.setCursor(17, 3);
-  oled.println(sensor_inhouse.getTempFByIndex(0));
-       /////Celcius/////
-//   oled.setCursor(15, 0);
-//   oled.print("degrees");
-//   oled.setCursor(15, 1);
-//   oled.print("celcius:");
-//   oled.setCursor(13, 3);
-//   oled.print(sensor_inhouse.getTempCByIndex(0));
- }
+void loop(void){ 
+  // Choose Either Fahrenheit or Celsius With 5x5FONT //
+          /////Fahrenheit/////
+  //oled.setCursor(15, 0); //5x5FONT
+  oled.setCursor(22, 0);
+  oled.print("Degrees");
+  //oled.setCursor(4, 1);  //5x5FONT
+  //oled.print("Fahrenheit:");  //5x5FONT
+  //oled.setCursor(15, 3);  //5x5FONT
+  oled.setCursor(22, 2);
+  oled.print(sensors.getTempFByIndex(0));
+  //oled.setCursor(48, 3);  //5x5FONT
+  oled.println(" °F");
+           /////Celcius/////
+  //oled.setCursor(15, 0);  //5x5FONT
+  //oled.print("Degrees");  //5x5FONT
+  //oled.setCursor(15, 1);  //5x5FONT
+  //oled.print("Celcius:");  //5x5FONT
+  oled.setCursor(22, 3);  
+  oled.print(sensors.getTempCByIndex(0));
+  //oled.setCursor(48, 3);  //5x5FONT
+  oled.println(" °C");
+
+}
